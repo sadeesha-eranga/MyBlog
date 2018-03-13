57918edfb37a9e1f68a387ecf5b85998fd591ba4
Sadeesha Eranga <msadeeshaeranga@gmail.com>
2018-03-13 16:53:49 +0530

    Front-end done...

--- js/admin-panel-controller.js
+++ js/admin-panel-controller.js
@@ -1,3 +1,6 @@
+
+$(document).ajaxStart(function() { Pace.restart(); });
+
 $('#btnSignOut').click(function () {
 
     swal({
@@ -6,11 +9,17 @@ $('#btnSignOut').click(function () {
             type: "warning",
             showCancelButton: true,
             confirmButtonClass: "btn-warning",
-            confirmButtonText: "Yes!",
+            confirmButtonText: "Yes, Go to main site.",
             closeOnConfirm: false
         },
         function () {
             window.location.href = "index.html";
         });
+
+});
+
+$('#btnProfile').click(function () {
+
+    window.location.href = "edit-profile.html";
 
 });
---
+++ js/edit-profile-controller.js
@@ -0,0 +1,17 @@
+$("#btnUpdate").click(function () {
+
+    console.log($('#txtPost').val());
+    console.log($('#txtName').val());
+    console.log($('#txtPassword').val());
+    console.log($('#txtDescription').val());
+    console.log($('#txtEmail').val());
+    console.log($('#txtTitle').val());
+    console.log($('#txtUsername').val());
+
+});
+
+$("#changeProfilePic").click(function () {
+
+    $("#fileSelector").click();
+
+});
